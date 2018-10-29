1649c69e1888a066e55b1238535b7916e7e968a4
Sandy <sedwardscode@gmail.com>
2018-10-29 15:12:08 -0400

    first commit

---
+++ src/components/someRandomComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class someRandomComponent extends React.Component {
+
+  render() {
+    return(
+      <div className="random-component">
+        
+      </div>
+    )
+  }
+};
